3bd08642ad3b80dd836efa1d9ef9d18c174c2c3c
jiangliang <jiangliang.net@gmail.com>
2019-08-27 18:15:27 +0800

    黑屏问题

--- lib/flutter_i18n_delegate.dart
+++ lib/flutter_i18n_delegate.dart
@@ -7,14 +7,9 @@ import 'package:flutter/material.dart';
 import 'flutter_i18n.dart';
 
 class FlutterI18nDelegate extends LocalizationsDelegate<FlutterI18n> {
-  final bool useCountryCode;
-  final String fallbackFile;
-  final String path;
-  final FlutterI18n flutterI18n ;
-  FlutterI18nDelegate(
-      {this.useCountryCode = false,
-      this.fallbackFile,
-      this.path = "assets/flutter_i18n",this.flutterI18n});
+
+  final FlutterI18n flutterI18n;
+  FlutterI18nDelegate(this.flutterI18n);
 
   @override
   bool isSupported(final Locale locale) {
@@ -23,6 +18,7 @@ class FlutterI18nDelegate extends LocalizationsDelegate<FlutterI18n> {
 
   @override
   Future<FlutterI18n> load(final Locale locale) async {
+
     return flutterI18n;
   }
 
